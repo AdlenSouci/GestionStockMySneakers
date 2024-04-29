11fdf3d7ac7355f97e052327ed39ac18f60fcf3e
adlen souci <77266719+AdlenSouci@users.noreply.github.com>
2024-04-29 10:59:37 +0200

    new

--- Pages/consulter.xaml.cs
+++ Pages/consulter.xaml.cs
@@ -1,76 +1,68 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace GestionStockMySneakers.Pages
 {
+    public class Article
+    {
+        public string img { get; set; }
+        public string marque { get; set; }
+        public string nom_famille { get; set; }
+        public string modele { get; set; }
+        public string description { get; set; }
+        public string couleur { get; set; }
+        public string prix_public { get; set; }
+        public string prix_achat { get; set; }
+    }
+
     public partial class consulter : Page
     {
+        // Objets nécessaires pour SQL
+        const string _dsn = "server=localhost;port=3306;database=projet_sneakers;username=root;password=;";
+        private MySqlConnection _connexion = new MySqlConnection(_dsn);
+        private MySqlCommand _command;
+
         public consulter()
         {
             InitializeComponent();
-
-            List<string> files = GetFileList("ftp://ftp.kuph3194.odns.fr/resources/img/", "sneakers@ifcsio2022.fr", "5NRN7lbrz3Zt");
-            listBox.ItemsSource = files;
+            afficher();
         }
 
-        public List<string> GetFileList(string server, string user, string password)
+        private void afficher()
         {
-            List<string> files = new List<string>();
             try
             {
-                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(server);
-                request.Method = WebRequestMethods.Ftp.ListDirectory;
-                request.Credentials = new NetworkCredential(user, password);
+                _command = new MySqlCommand("SELECT * FROM articles;", _connexion);
+                _connexion.Open();
+                MySqlDataReader reader = _command.ExecuteReader();
 
-                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                List<Article> articles = new List<Article>();
+                while (reader.Read())
                 {
-                    string line = reader.ReadLine();
-                    while (line != null)
+                    articles.Add(new Article
                     {
-                        // Download the file and save it locally
-                        string localFilePath = DownloadFile(server + "/" + line, user, password, line);
-                        files.Add(localFilePath);
-
-                        line = reader.ReadLine();
-                    }
+                        img = reader["img"].ToString(),
+                        marque = reader["marque"].ToString(),
+                        nom_famille = reader["nom_famille"].ToString(),
+                        modele = reader["modele"].ToString(),
+                        description = reader["description"].ToString(),
+                        couleur = reader["couleur"].ToString(),
+                        prix_public = reader["prix_public"].ToString(),
+                        prix_achat = reader["prix_achat"].ToString()
+                    });
                 }
+
+                icArticles.ItemsSource = articles;  
+
+                _connexion.Close();
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
-            }
-
-            return files;
-        }
-
-        public string DownloadFile(string serverFilePath, string user, string password, string fileName)
-        {
-            try
-            {
-                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(serverFilePath);
-                request.Method = WebRequestMethods.Ftp.DownloadFile;
-                request.Credentials = new NetworkCredential(user, password);
-
-                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
-                using (Stream responseStream = response.GetResponseStream())
-                using (FileStream writer = new FileStream(fileName, FileMode.Create))
-                {
-                    responseStream.CopyTo(writer);
-                }
-
-                return Path.GetFullPath(fileName);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return null;
+                MessageBox.Show(ex.Message);
             }
         }
-
     }
 }
