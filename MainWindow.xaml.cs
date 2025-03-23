6ac0a79c8fede6431663bc5ef349d666d8904fa7
Philippe <philippe.kurz@gmail.com>
2025-03-23 21:17:51 +0100

    Main menu et nouvelles pages

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace GestionStockMySneakers
         }
 
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_Accueil(object sender, RoutedEventArgs e)
         {
 
             MainWindow mainWindow = new MainWindow();
@@ -46,22 +46,22 @@ namespace GestionStockMySneakers
             this.Close();
         }
 
-        private void MenuItem_Click_A(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_Articles(object sender, RoutedEventArgs e)
         {
             MainFrame.NavigationService.Navigate(new Uri("Pages/Articles.xaml", UriKind.Relative));
         }
 
-        private void MenuItem_Click_F(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_Familles(object sender, RoutedEventArgs e)
         {
             MainFrame.NavigationService.Navigate(new Uri("Pages/Familles.xaml", UriKind.Relative));
         }
 
-        private void MenuItem_Click_M(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_Marques(object sender, RoutedEventArgs e)
         {
             MainFrame.NavigationService.Navigate(new Uri("Pages/Marques.xaml", UriKind.Relative));
         }
 
-        private void MenuItem_Click_C(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_Couleurs(object sender, RoutedEventArgs e)
         {
             MainFrame.NavigationService.Navigate(new Uri("Pages/Couleurs.xaml", UriKind.Relative));
         }
@@ -71,37 +71,22 @@ namespace GestionStockMySneakers
             MainFrame.NavigationService.Navigate(new Uri("Pages/Avis.xaml", UriKind.Relative));
         }
 
-        private void MenuItem_Click_Consulter(object sender, RoutedEventArgs e)
-        {
-            MainFrame.NavigationService.Navigate(new Uri("Pages/consulter.xaml", UriKind.Relative));
-        }
-
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void MenuItem_Click_Gerer(object sender, RoutedEventArgs e)
-        {
-            MainFrame.NavigationService.Navigate(new Uri("Pages/Stock.xaml", UriKind.Relative));
-        }
-
-        private void MenuItem_Click_Com(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_Commandes(object sender, RoutedEventArgs e)
         {
             MainFrame.NavigationService.Navigate(new Uri("Pages/Commande.xaml", UriKind.Relative));
         }
 
-        private void MenuItem_Click_8(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_APropos(object sender, RoutedEventArgs e)
         {
-            MainFrame.NavigationService.Navigate(new Uri("Pages/Commande.xaml", UriKind.Relative));
+            MainFrame.NavigationService.Navigate(new Uri("Pages/APropos.xaml", UriKind.Relative));
         }
 
-        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        private void MenuItem_Click_Parametres(object sender, RoutedEventArgs e)
         {
-
+            MainFrame.NavigationService.Navigate(new Uri("Pages/Parametres.xaml", UriKind.Relative));
         }
 
-        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_Quitter(object sender, RoutedEventArgs e)
         {
             Close();
         }
